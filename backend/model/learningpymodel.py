145e1c1995b5086debaffe5721f387683c45b999
VedShewale <ved.shewale11@gmail.com>
2026-02-24 17:59:34 +0100

    Imported necessary stuff, renamed model to learning, using it for learning pytorch

---
+++ backend/model/learningpymodel.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+import timm
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
